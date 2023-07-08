ff5c97dd44a802fae2d359ba407d27955d312d19
aditya19991010 <66785328+aditya19991010@users.noreply.github.com>
2023-07-08 19:24:44 +0530

    03_trimmomatic.sh

--- ngs/03_trimmomatic.sh
+++ ngs/03_trimmomatic.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+#trimming 13 nucleotide from start of reads
+
 # Define an array of file names
 files=(
   "SRR6660738"
@@ -19,6 +22,7 @@ for file in "${files[@]}"; do
   output_file_1="${file}_2_trim.fastq"
   input_file_2="${file}_2.fastq"
   output_file_2="${file}_2_trim.fastq"
+  
   # Run trimmomatic to remove the first 13 bases
   java -jar /home/aditya/software/Trimmomatic/trimmomatic-0.39.jar PE -threads 40 -phred33 "${input_file_1}" "${input_file_2}" "${output_file_1}_1P" "${output_file_1}_1U" "${output_file_2}_2P" "${output_file_1}_2U" HEADCROP:13
 done
