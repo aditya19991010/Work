db377078fc4d96f7bb3ee7293e7ad0440eaef0d9
aditya19991010 <66785328+aditya19991010@users.noreply.github.com>
2023-07-08 19:02:30 +0530

    Blastn_multi_Seq_script.sh
    blastx will run for each query file sequencially givin in the array generated in 1kp_all_file_name.txt

--- Blastn_multi_Seq_script.sh
+++ Blastn_multi_Seq_script.sh
@@ -7,11 +7,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
 # Get into directories
-cd /path/to/1KP_address_file
-cd = 
+cd  = /path/to/1KP_address_file
+
+#blastx will run for each query file sequencially givin in the array generated in 1kp_all_file_name.txt
 
 # Loop through the text file
 for i in 1kp_all_file_name.txt ; do
-  # Run the MEME program with the input file and output directory names
+  # Run the blastx program with the input query and database directory names
   blastx -query $i -db /media/lgi/Disk1/professional/Software/ -out "$i/fa"xml -evalue 1e-10 -outfmt 5 -num_threads 40 
 done
